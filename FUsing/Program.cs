81de90b037b546319a469b3ce7f5eccb29e235e8
MachieHacker <HacunaBoy@protonmail.com>
2021-09-22 20:53:53 +0300

    Домашнее задание 1

---
+++ FUsing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BubbleSort;
+
+namespace FUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int[] array = new int[]{5,27,96,62,547,23,654,123,5,4,2,64,53,5123};
+            int[] res = BubbleSort.ShareSort.sort(array);
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.Write(res[i] + " ");
+            }
+        }
+    }
+}
